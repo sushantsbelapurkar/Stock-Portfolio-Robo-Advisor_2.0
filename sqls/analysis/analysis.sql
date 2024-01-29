29fb80d372d82a79998e952664509b8eaf5b9138
Sushant Belapurkar <sushantsbelapurkar@gmail.com>
2024-01-28 20:49:08 -0800

    modified analysis queries to study data

--- sqls/analysis/analysis.sql
+++ sqls/analysis/analysis.sql
@@ -1,12 +1,19 @@
-select * from mysql_portfolio.vw_stock_parameter_check limit 10;
+select * from mysql_portfolio.symbol_list where name like '%ORIENTAL%' limit 10;
+SELECT * FROM mysql_portfolio.vw_stock_parameter_check limit 10;
+-- FUNDAMENTAL TOTAL Range	71 -->	53 -->	22
+-- GROWTH TOTAL	Range       68 -->	47 -->	28
+select * from mysql_portfolio.company_score
+where fundamental_score >= 53 and growth_score >= 47;
 
 -- portfolio VALUE analysis, score and api rating
 select
 portfolio.stock,portfolio.symbol,portfolio.exchange,
 portfolio.latest_price, portfolio.quantity,
 portfolio.investment_price,
-stock_view.company_cap,stock_view.industry,stock_view.sector,stock_view.beta,
-stock_view.freeCashFlow,stock_view.grahamNumber,
+stock_view.company_cap,stock_view.industry,stock_view.sector,stock_view.beta, stock_view._5yr_avg_eps,
+stock_view.freeCashFlow,(15*stock_view._5yr_avg_eps) as fair_value,
+stock_view.grahamNumber, (((15*stock_view._5yr_avg_eps) + stock_view.grahamNumber)/2) avg_fair_price,
+stock_view._50day_avg_price, stock_view._200day_avg_price,
 analysis.calendarYear,analysis.current_ratio_analysis,analysis.debt_to_equity_analysis,
 analysis.inventory_analysis,analysis.is_roic_greater_wacc,analysis.pb_ratio_analysis,
 analysis.pe_and_sector_pe_analysis,analysis.pepb_ratio_analysis,analysis.pe_ratio_analysis,
@@ -34,8 +41,10 @@ select
 portfolio.stock,portfolio.symbol,portfolio.exchange,
 portfolio.latest_price, portfolio.quantity,
 portfolio.investment_price,
-stock_view.company_cap,stock_view.industry,stock_view.sector,stock_view.beta,
-stock_view.freeCashFlow,stock_view.grahamNumber,
+stock_view.company_cap,stock_view.industry,stock_view.sector,stock_view.beta, stock_view._5yr_avg_eps,
+stock_view.freeCashFlow,(15*stock_view._5yr_avg_eps) as fair_value,
+stock_view.grahamNumber, (((15*stock_view._5yr_avg_eps) + stock_view.grahamNumber)/2) avg_fair_price,
+stock_view._50day_avg_price, stock_view._200day_avg_price,
 analysis.calendarYear,analysis.capex_growth_ofc,analysis.capex_growth_revenue,
 analysis.divident_growth,analysis.ebitda_growth,analysis.eps_growth_analysis,
 analysis.is_roic_greater_wacc,analysis.netincome_growth,analysis.operating_cash_flow_growth,
@@ -60,8 +69,10 @@ order by growth_score desc
 -- Overall VALUE analysis, score and api rating
 select
 analysis.symbol,
-stock_view.company_cap,stock_view.industry,stock_view.sector,stock_view.beta,
-stock_view.freeCashFlow,stock_view.grahamNumber,
+stock_view.company_cap,stock_view.industry,stock_view.sector,stock_view.beta, stock_view._5yr_avg_eps,
+stock_view.freeCashFlow, (15*stock_view._5yr_avg_eps) as fair_value,
+stock_view.grahamNumber, (((15*stock_view._5yr_avg_eps) + stock_view.grahamNumber)/2) avg_fair_price,
+stock_view._50day_avg_price, stock_view._200day_avg_price,
 analysis.calendarYear,analysis.current_ratio_analysis,analysis.debt_to_equity_analysis,
 analysis.inventory_analysis,analysis.is_roic_greater_wacc,analysis.pb_ratio_analysis,
 analysis.pe_and_sector_pe_analysis,analysis.pepb_ratio_analysis,analysis.pe_ratio_analysis,
@@ -79,15 +90,19 @@ left join mysql_portfolio.api_rating
 on api_rating.symbol = analysis.symbol
 left join mysql_portfolio.vw_stock_parameter_check stock_view
 on stock_view.symbol = analysis.symbol
-WHERE fundamental_score >= 53
+WHERE
+-- analysis.symbol like '%BRK%'
+fundamental_score >= 53
 and analysis.symbol not in (select distinct symbol from mysql_portfolio.portfolio_stocks where latest_price !=0)
 order by fundamental_score desc
 ;
 -- Overall GROWTH analysis, score and api rating
 select
 analysis.symbol,
-stock_view.company_cap,stock_view.industry,stock_view.sector,stock_view.beta,
-stock_view.freeCashFlow,stock_view.grahamNumber,
+stock_view.company_cap,stock_view.industry,stock_view.sector,stock_view.beta, stock_view._5yr_avg_eps,
+stock_view.freeCashFlow, (15*stock_view._5yr_avg_eps) as fair_value,
+stock_view.grahamNumber, (((15*stock_view._5yr_avg_eps) + stock_view.grahamNumber)/2) avg_fair_price,
+stock_view._50day_avg_price, stock_view._200day_avg_price,
 analysis.calendarYear,analysis.capex_growth_ofc,analysis.capex_growth_revenue,
 analysis.divident_growth,analysis.ebitda_growth,analysis.eps_growth_analysis,
 analysis.is_roic_greater_wacc,analysis.netincome_growth,analysis.operating_cash_flow_growth,
